b0f32c182e11b9144404fb0fb93c119d02dace3d
YousefEZ <45167695+YousefEZ@users.noreply.github.com>
2021-01-04 21:33:04 +0000

    Add files via upload

---
+++ utils/emojis.py
@@ -0,0 +1,33 @@
+PAGES = {1: "1\u20e3",
+         2: "2\u20e3",
+         3: "3\u20e3",
+         4: "4\u20e3",
+         5: "5\u20e3",
+         6: "6\u20e3",
+         7: "7\u20e3",
+         8: "8\u20e3",
+         9: "9\u20e3"}
+
+ARROW_RIGHT = "\U000027a1"
+CHECK_MARK = "\U00002705"
+CANCEL = "\U0000274c"
+PAUSE = "\U000023f8"
+REFRESH = "\U0001f504"
+EXAMINE = "\U0001f50d"
+
+# PERSEUS
+BLUEPRINT = '<:Blueprint:694918033658871890>'
+UPGRADE = '<:upgrade:749630608745037865>'
+DOWNGRADE = '<:downgrade:749631005815603292>'
+
+# LAND VEHICLES EMOJIS
+TANK = "<:tank:697492834214740078>"
+HOWITZER = "<:rocket_artillery:697878449708335114>"
+ROCKET = "<:rocket_artillery:697878449708335114>"
+ANTIAIR = "<:antiair:697880045359857686>"
+APC = "<:APC:697941924954505257>"
+
+# NAVAL EMOJIS
+WARSHIP = "<:warship:705370162165186561>"
+SUBMARINE = "<:submarine:707970679169089637>"
+CARRIER = "<:carrier:707971240530411643>"
